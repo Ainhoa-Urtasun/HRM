7765d80540486a529b8607f6eeb17577d4fb41c4
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-10-14 07:35:35 +0200

    Update UNIT2.py

--- APP/pages/UNIT2.py
+++ APP/pages/UNIT2.py
@@ -11,7 +11,7 @@ def UNIT2_1():
         r"O*NET Online is a comprehensive tool for career exploration and job analysis. "
         "It offers detailed descriptions of the tasks, skills, and other attributes required for more than 1,000 jobs, "
         "making it an invaluable resource for job seekers, workforce development professionals, and HR specialists. "
-        "By providing standardized information on various job roles, O*NET Online facilitates job analysis and design, "
+        r"By providing standardized information on various job roles, O*NET Online facilitates job analysis and design, "
         "helping users understand the specific demands and characteristics of different occupations."
     )
 
