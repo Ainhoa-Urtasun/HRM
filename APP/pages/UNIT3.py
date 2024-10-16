0abfbb808e498f973f68e25826ecc1db0eeb6a87
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-10-16 20:08:42 +0200

    Update UNIT3.py

--- APP/pages/UNIT3.py
+++ APP/pages/UNIT3.py
@@ -1,1 +1,34 @@
-dfdfd
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def UNIT2_1():
+st.write("### Definitions")
+
+st.write("**Employment**: The total number of people currently working for the firm, including both full-time and part-time employees.")
+
+st.write("**New Hires**: The number of individuals who have recently been recruited and started working at the firm.")
+
+st.write("**Job Postings**: Advertisements made by the firm to fill open positions, specifying required qualifications and job responsibilities.")
+
+st.write("**Separations**: The total number of employees who leave the firm, either voluntarily (quitting, retiring) or involuntarily (layoffs, dismissals).")
+
+
+
+st.set_page_config(page_title="UNIT3", layout="wide")
+
+selected = option_menu(
+    menu_title="Main Menu",  # required
+    options=["Terminology'],  # required
+    icons=["house", "book", "calculator", "person", "globe"],  # optional
+    menu_icon="cast",  # optional
+    default_index=0,  # optional
+    orientation="vertical",
+)
+
+# Call the selected section
+if selected == "Terminology":
+    UNIT3_1()
+
