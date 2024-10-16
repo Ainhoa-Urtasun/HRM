79bf1e3a24d5afd12a9a7cdd96a8f8090a70ee09
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-10-16 20:09:48 +0200

    Update UNIT3.py

--- APP/pages/UNIT3.py
+++ APP/pages/UNIT3.py
@@ -5,17 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def UNIT2_1():
-st.write("### Definitions")
-
-st.write("**Employment**: The total number of people currently working for the firm, including both full-time and part-time employees.")
-
-st.write("**New Hires**: The number of individuals who have recently been recruited and started working at the firm.")
-
-st.write("**Job Postings**: Advertisements made by the firm to fill open positions, specifying required qualifications and job responsibilities.")
-
-st.write("**Separations**: The total number of employees who leave the firm, either voluntarily (quitting, retiring) or involuntarily (layoffs, dismissals).")
-
-
+    st.write("**Employment**: The total number of people currently working for the firm, including both full-time and part-time employees.")
+    st.write("**New Hires**: The number of individuals who have recently been recruited and started working at the firm.")
+    st.write("**Job Postings**: Advertisements made by the firm to fill open positions, specifying required qualifications and job responsibilities.")
+    st.write("**Separations**: The total number of employees who leave the firm, either voluntarily (quitting, retiring) or involuntarily (layoffs, dismissals).")
 
 st.set_page_config(page_title="UNIT3", layout="wide")
 
