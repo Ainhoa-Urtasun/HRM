3c8c956255870a35ca7256c24d20a2a43a89f7f1
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-10-20 08:30:02 +0200

    Update UNIT2.py

--- APP/pages/UNIT2.py
+++ APP/pages/UNIT2.py
@@ -52,11 +52,20 @@ def UNIT2_3():
 
 def UNIT2_4():
     st.write(
-    "In the context of job analysis and design, it is useful to represent a work activity as a vector of skills:"
+    "In the context of job analysis and design, it is useful to represent a work activity as a column-vector of skills:"
     )
     
-    st.latex(r'W_i = (s_{i1}, s_{i2}, s_{i3}, s_{i4})')
-
+    st.latex(r
+             '''
+             W_i  = 
+             \begin{pmatrix}
+             S_{1i} \\
+             S_{2i} \\ 
+             S_{3i} \\ 
+             S_{4i} \\
+             \end{matrix}
+            '''
+    
     st.write(
     "Each element $s_{ij}$ of the vector indicates the extent to which skill $j$ is required for work activity $i$:"
     )    
