bd7eb2bcb12adf9f896ca0436e8a240cc4d84abe
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-10-30 15:25:02 +0100

    Update UNIT1.py

--- APP/pages/UNIT1.py
+++ APP/pages/UNIT1.py
@@ -56,13 +56,13 @@ def UNIT1_2():
     )
 
     st.markdown("<h3 style='color: #4CAF50;'>🚀 HRM Analytics</h3>", unsafe_allow_html=True)
-    st.markdown(
-    '''
-    Select the sector into which you want your firm to be classified using NACE at 
-    [Skills Intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence).
-    '''
+    st.text_input(
+        '''
+        Select the sector into which you want your firm to be classified using NACE at 
+        [Skills Intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence).
+        Write here the industry name and economic activities:
+        '''
     )
-    st.text_input("Industry name and economic activities:")
     st.markdown(
     '''
     Select 3 jobs for your firm using ISCO occupations at 
