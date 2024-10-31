5d3a050dec38511315f13a36ed46a53e26913a06
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-10-31 06:16:57 +0100

    Update UNIT7.py

--- APP/pages/UNIT7.py
+++ APP/pages/UNIT7.py
@@ -28,7 +28,7 @@ def UNIT7_2():
     st.write(
         '''
         Consider two employees, employee 1 and employee 2, competing for a prize $w$. The winner receives $w$, and the loser receives $-w$. 
-        The probability of winning for Employee 1 is given by:
+        The probability of winning for employee 1 and for employee 2 is given by, respectively:
         '''
     )
     
@@ -38,8 +38,58 @@ def UNIT7_2():
         '''
     )
 
-  
+    st.latex(
+        r'''
+        p_2 = \frac{e_2}{e_1 + e_2}
+        '''
+    )
 
+    st.write(
+        '''
+        The expected utility for each employee depends on their probability of winning, the prize, and the cost of effort. 
+        Let \( C(e_i) = d_i e_i^2 \) represent the cost of effort for each employee.
+
+        '''
+    )
+
+
+    st.latex(
+        r'''
+        U_1 = \frac{e_1}{e_1 + e_2} w - \frac{e_2}{e_1 + e_2} w - d_1 e_1^2 \\[10pt]
+        U_2 = \frac{e_2}{e_1 + e_2} w - \frac{e_1}{e_1 + e_2} w - d_2 e_2^2
+        '''
+    )
+
+    st.write(
+        '''
+        Each employee chooses their effort level to maximize their expected utility. We find the optimal effort levels by 
+        taking the first-order conditions of their expected utility functions with respect to their efforts.
+        '''
+    )
+
+    st.latex(
+        r'''
+        \frac{\partial U_1}{\partial e_1} = \frac{\partial}{\partial e_1} \left( \frac{e_1}{e_1 + e_2} w - \frac{e_2}{e_1 + e_2} w - d_1 e_1^2 \right) = 0
+        '''
+    )
+
+    st.latex(
+        r'''
+        \frac{\partial U_1}{\partial e_1} = \frac{w}{(e_1 + e_2)^2} e_2 - d_1 e_1 = 0
+        '''
+    )
+
+    st.latex(
+        r'''
+        \frac{\partial U_2}{\partial e_2} = \frac{\partial}{\partial e_2} \left( \frac{e_2}{e_1 + e_2} w - \frac{e_1}{e_1 + e_2} w - d_2 e_2^2 \right) = 0
+        '''
+    )
+
+    st.latex(
+        r'''
+        \frac{\partial U_2}{\partial e_2} = \frac{w}{(e_1 + e_2)^2} e_1 - d_2 e_2 = 0
+        '''
+    )
  
 st.set_page_config(page_title="UNIT7", layout="wide")
 
