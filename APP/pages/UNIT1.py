a047f8acfbb9d366ff7f784a03cf7747109b47ec
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-01 13:55:47 +0100

    Update UNIT1.py

--- APP/pages/UNIT1.py
+++ APP/pages/UNIT1.py
@@ -90,7 +90,7 @@ def UNIT1_3():
     )
 
     st.latex(r'''
-    \alpha_i = \frac{\partial f(e_1, e_2, \dots, e_i, \dots, e_L)}{\partial e_i} \cdot \frac{e_i}{f(e_1, e_2, \dots, e_i, \dots, e_L)}
+    \alpha_i = \frac{\partial Q}{\partial e_i} \cdot \frac{e_i}{Q}
     ''')
 
     st.write(
