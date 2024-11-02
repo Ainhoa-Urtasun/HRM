37521e93f65ac0c7df0784c1c894525727896898
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-02 08:12:04 +0100

    Update UNIT1.py

--- APP/pages/UNIT1.py
+++ APP/pages/UNIT1.py
@@ -56,7 +56,12 @@ def UNIT1_2():
     )
 
     st.markdown("<h3 style='color: #4CAF50;'>🚀 HRM Analytics</h3>", unsafe_allow_html=True)
-    st.text_input('Select the sector of your firm using NACE at [Skills Intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence). Write here the industry name and its economic activities:')
+    st.markdown(
+        '''
+        Select the sector of your firm from NACE rev. 2 at [Skills Intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence)
+        '''
+    )
+    st.text_input(placeholder='Write here the industry name and its economic activities:')
     st.markdown(
         """
         Select a firm using [SABI](https://www.unavarra.es/biblioteca?languageId=1):
@@ -65,9 +70,12 @@ def UNIT1_2():
         and professionals, and at least 5 women
         """
      )
-    firm_name = st.text_input("Write here the name of the firm:")
-    st.text_input('Select 3 jobs for your firm using ISCO occupations at [Skills Intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence). Write here the titles of the 3 jobs:',
-                   placeholder="A job is not the same as an occupation. Occupations are standardized, while jobs are defined by firms.")
+    st.text_input(placeholder='Write here the name of your firm:')
+    st.markdown(
+        '''
+        From ISCO at [Skills Intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence) precise the name of the 3 jobs at your firm
+        '''
+    st.text_input(placeholder="A job is not the same as an occupation. Occupations are standardized, while jobs are defined by firms.")
     
 def UNIT1_3():
     st.write(
