78680ee1b8b2161a8bf005722937391c51e26e65
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-06 00:07:51 +0100

    Update UNIT2.py

--- APP/pages/UNIT2.py
+++ APP/pages/UNIT2.py
@@ -115,31 +115,31 @@ def UNIT2_2():
     st.markdown("<h3 style='color: #4CAF50;'>🚀 HRM Analytics </h3>", unsafe_allow_html=True)
     st.sidebar.radio("Select a job at your firm:",("Senior management", "Support intellectuals and scientists, technicians and professionals", "Sales representatives and similar"))
     st.sidebar.write('Evaluate the job:')
-    with st.sidebar.expander("$t_1$ Intellectual"):
-        s11 = st.number_input("$s_{11k}$ Demonstrating willigness to learn",key='s11',step=1.0)
-        s21 = st.number_input("$s_{21k}$ Collaborating in teams and networks",key='s21',step=1.0)
-        s31 = st.number_input("$s_{31k}$ Working efficiently",key='s31',step=1.0)
-        s41 = st.number_input("$s_{41k}$ Taking a proactive approach",key='s41',step=1.0)
-    with st.sidebar.expander("$t_1$ Physical"):
-        s12 = st.number_input("$s_{12k}$ Demonstrating willigness to learn",key='s12',step=1.0)
-        s22 = st.number_input("$s_{22k}$ Collaborating in teams and networks",key='s22',step=1.0)
-        s32 = st.number_input("$s_{32k}$ Working efficiently",key='s32',step=1.0)
-        s42 = st.number_input("$s_{42k}$ Taking a proactive approach",key='s42',step=1.0)
-    with st.sidebar.expander("$t_1$ Social"):
-        s13 = st.number_input("$s_{13k}$ Demonstrating willigness to learn",key='s13',step=1.0)
-        s23 = st.number_input("$s_{23k}$ Collaborating in teams and networks",key='s23',step=1.0)
-        s33 = st.number_input("$s_{33k}$ Working efficiently",key='s33',step=1.0)
-        s43 = st.number_input("$s_{43k}$ Taking a proactive approach",key='s43',step=1.0)
-    with st.sidebar.expander("$t_1$ Use of methods"):
-        s14 = st.number_input("$s_{14k}$ Demonstrating willigness to learn",key='s14',step=1.0)
-        s24 = st.number_input("$s_{24k}$ Collaborating in teams and networks",key='s24',step=1.0)
-        s34 = st.number_input("$s_{34k}$ Working efficiently",key='s34',step=1.0)
-        s44 = st.number_input("$s_{44k}$ Taking a proactive approach",key='s44',step=1.0)
-    with st.sidebar.expander("$t_1$ Use of technology"):
-        s15 = st.number_input("$s_{15k}$ Demonstrating willigness to learn",key='s15',step=1.0)
-        s25 = st.number_input("$s_{25k}$ Collaborating in teams and networks",key='s25',step=1.0)
-        s35 = st.number_input("$s_{35k}$ Working efficiently",key='s35',step=1.0)
-        s45 = st.number_input("$s_{45k}$ Taking a proactive approach",key='s45',step=1.0)
+    with st.sidebar.expander("Intellectual"):
+        s11 = st.number_input("Demonstrating willigness to learn",key='s11',step=1.0)
+        s21 = st.number_input("Collaborating in teams and networks",key='s21',step=1.0)
+        s31 = st.number_input("Working efficiently",key='s31',step=1.0)
+        s41 = st.number_input("Taking a proactive approach",key='s41',step=1.0)
+    with st.sidebar.expander("Physical"):
+        s12 = st.number_input("Demonstrating willigness to learn",key='s12',step=1.0)
+        s22 = st.number_input("Collaborating in teams and networks",key='s22',step=1.0)
+        s32 = st.number_input("Working efficiently",key='s32',step=1.0)
+        s42 = st.number_input("Taking a proactive approach",key='s42',step=1.0)
+    with st.sidebar.expander("Social"):
+        s13 = st.number_input("Demonstrating willigness to learn",key='s13',step=1.0)
+        s23 = st.number_input("Collaborating in teams and networks",key='s23',step=1.0)
+        s33 = st.number_input("Working efficiently",key='s33',step=1.0)
+        s43 = st.number_input("Taking a proactive approach",key='s43',step=1.0)
+    with st.sidebar.expander("Use of methods"):
+        s14 = st.number_input("Demonstrating willigness to learn",key='s14',step=1.0)
+        s24 = st.number_input("Collaborating in teams and networks",key='s24',step=1.0)
+        s34 = st.number_input("Working efficiently",key='s34',step=1.0)
+        s44 = st.number_input("Taking a proactive approach",key='s44',step=1.0)
+    with st.sidebar.expander("Use of technology"):
+        s15 = st.number_input("Demonstrating willigness to learn",key='s15',step=1.0)
+        s25 = st.number_input("Collaborating in teams and networks",key='s25',step=1.0)
+        s35 = st.number_input("Working efficiently",key='s35',step=1.0)
+        s45 = st.number_input("Taking a proactive approach",key='s45',step=1.0)
 
     matrix = np.array([
             [s11, s12, s13, s14, s15],
