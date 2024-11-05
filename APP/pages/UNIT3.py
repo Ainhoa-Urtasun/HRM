e73e081f4ba1f9b31eeb67f524242258fe2c0bde
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-05 20:36:19 +0100

    Update UNIT3.py

--- APP/pages/UNIT3.py
+++ APP/pages/UNIT3.py
@@ -51,13 +51,13 @@ def UNIT3_1():
     st.markdown("<h3 style='color: #4CAF50;'>🚀 HRM Analytics </h3>", unsafe_allow_html=True)
     st.sidebar.write('Data collection:')
     with st.sidebar.expander("$J_{(1)}$ Senior management"):
-        L1past = st.number_input("$L_{(1,-1)}$", key='L1past', step=1, min_value=2)
+        L1past = st.number_input("$L_{(1,-1)}$", key='L1past', step=1, min_value=1)
         L1present = st.number_input("$L_{(1,0)}$", key='L1present', step=1, min_value=0)
     with st.sidebar.expander("$J_{(2)}$ Support intellectuals and scientists, technicians and professionals"):
-        L2past = st.number_input("$L_{(2,-1)}$", key='L2past', step=1, min_value=10)
+        L2past = st.number_input("$L_{(2,-1)}$", key='L2past', step=1, min_value=1)
         L2present = st.number_input("$L_{(2,0)}$", key='L2present', step=1, min_value=0)
     with st.sidebar.expander("$J_{(3)}$ Sales representatives and similar"):
-        L3past = st.number_input("$L_{(3,-1)}$", key='L3past', step=1, min_value=5)
+        L3past = st.number_input("$L_{(3,-1)}$", key='L3past', step=1, min_value=1)
         L3present = st.number_input("$L_{(3,0)}$", key='L3present', step=1, min_value=0)
 
     def distribute_values(Lpast, Lpresent, seed):
