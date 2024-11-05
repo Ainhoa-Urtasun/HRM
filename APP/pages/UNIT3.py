efea30e0fbd22b3bd3f267c896cb2f70257597d2
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-05 20:34:05 +0100

    Update UNIT3.py

--- APP/pages/UNIT3.py
+++ APP/pages/UNIT3.py
@@ -52,13 +52,13 @@ def UNIT3_1():
     st.sidebar.write('Data collection:')
     with st.sidebar.expander("$J_{(1)}$ Senior management"):
         L1past = st.number_input("$L_{(1,-1)}$", key='L1past', step=1, min_value=3)
-        L1present = st.number_input("$L_{(1,0)}$", key='L1present', step=1, min_value=3)
+        L1present = st.number_input("$L_{(1,0)}$", key='L1present', step=1, min_value=0)
     with st.sidebar.expander("$J_{(2)}$ Support intellectuals and scientists, technicians and professionals"):
         L2past = st.number_input("$L_{(2,-1)}$", key='L2past', step=1, min_value=10)
-        L2present = st.number_input("$L_{(2,0)}$", key='L2present', step=1, min_value=10)
+        L2present = st.number_input("$L_{(2,0)}$", key='L2present', step=1, min_value=0)
     with st.sidebar.expander("$J_{(3)}$ Sales representatives and similar"):
         L3past = st.number_input("$L_{(3,-1)}$", key='L3past', step=1, min_value=5)
-        L3present = st.number_input("$L_{(3,0)}$", key='L3present', step=1, min_value=5)
+        L3present = st.number_input("$L_{(3,0)}$", key='L3present', step=1, min_value=0)
 
     def distribute_values(Lpast, Lpresent, seed):
         random.seed(seed)
