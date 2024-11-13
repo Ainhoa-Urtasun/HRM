416395688da9d64ad6af1d20bdf8492483a79fe1
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-13 15:57:00 +0100

    Update UNIT 4 Employee Performance Evaluation.py

--- APP/pages/UNIT 4 Employee Performance Evaluation.py
+++ APP/pages/UNIT 4 Employee Performance Evaluation.py
@@ -46,30 +46,6 @@ def UNIT4_2():
     st.latex(r'\text{Labor productivity} = \frac{pQ}{L}')
     st.latex(r'\text{ULC} = \frac{wL}{pQ}')
 
-    st.markdown("<h3 style='color: #4CAF50;'>🚀 HRM Analytics </h3>", unsafe_allow_html=True)
-    with st.sidebar.expander("Cost of employees at your firm from [SABI](https://www.unavarra.es/biblioteca?languageId=1)"):
-        C2020 = st.number_input("2020",key='C2020',step=1.0)
-        C2021 = st.number_input("2021",key='C2021',step=1.0)
-        C2022 = st.number_input("2022",key='C2022',step=1.0)
-    with st.sidebar.expander("Operating revenue of your firm from [SABI](https://www.unavarra.es/biblioteca?languageId=1)"):
-        OR2020 = st.number_input("2020",key='OR2020',value=1.0,step=1.0)
-        OR2021 = st.number_input("2021",key='OR2021',value=1.0,step=1.0)
-        OR2022 = st.number_input("2022",key='OR2022',value=1.0,step=1.0)
-    with st.sidebar.expander("Number of employees at your firm from [SABI](https://www.unavarra.es/biblioteca?languageId=1)"):
-        L2020 = st.number_input("2020",key='L2020',value=1.0,step=1.0)
-        L2021 = st.number_input("2021",key='L2021',value=1.0,step=1.0)
-        L2022 = st.number_input("2022",key='L2022',value=1.0,step=1.0)
-
-    LP = [OR2020/L2020,OR2021/L2021,OR2022/L2022]
-    ULC = [C2020/OR2020,C2021/OR2021,C2022/OR2022]
-
-    fig = plt.figure(figsize=(5,5),dpi=100)
-    plt.plot(['2020','2021','2022'],LP,color='red',label='Labor productivity')
-    plt.plot(['2020','2021','2022'],ULC,color='blue',label='ULC')
-    plt.title('HRM metrics over time')
-    plt.legend()
-    st.pyplot(fig)
-    
 def UNIT4_3():
 
     st.write(
@@ -105,60 +81,8 @@ def UNIT4_3():
         the firm produces when employee $i$ exerts 1% more effort.
         '''
     )
+
 def UNIT4_4():
-    
-    st.write(
-        '''
-        The **skill gap** between an employee and the job they are performing can be 
-        measured by he Euclidean distance between the **job skill requirements** $s_k$ 
-        and the **employee skill profile** $s_i = (s_{i1}, s_{i2}, s_{i3}, s_{i4})$:
-        '''
-    )
-
-    st.latex(
-        r"""
-        g_i = \sqrt{(\|s_{1k}\| - s_{1i})^2 + (\|s_{2k}\| - s_{2i})^2 + (\|s_{3k}\| - s_{3i})^2 + (\|s_{4k}\| - s_{4i})^2}
-        """
-    )
-
-    st.write(
-        '''
-        As a performance evaluation metric, the **skill gap** is useful for decisions on
-        recruitment, training, and compensation.
-        '''
-    )
-
-    st.markdown("<h3 style='color: #4CAF50;'>🚀 HRM Analytics </h3>", unsafe_allow_html=True)
-    st.sidebar.multiselect("Select two tasks of the job at your firm:",("Intellectual","Physical","Social","Use of methods","Use of technology"))
-    st.sidebar.write('Evaluate the job:')
-    with st.sidebar.expander("$t_i$"):
-        s1i = st.number_input("$s_{1i(k)}$ Demonstrating willigness to learn",key='s1i',step=1.0)
-        s2i = st.number_input("$s_{2i(k)}$ Collaborating in teams and networks",key='s2i',step=1.0)
-        s3i = st.number_input("$s_{3i(k)}$ Working efficiently",key='s3i',step=1.0)
-        s4i = st.number_input("$s_{4i(k)}$ Taking a proactive approach",key='s4i',step=1.0)
-    with st.sidebar.expander("$t_j$"):
-        s1j = st.number_input("$s_{1j(k)}$ Demonstrating willigness to learn",key='s1j',step=1.0)
-        s2j = st.number_input("$s_{2j(k)}$ Collaborating in teams and networks",key='s2j',step=1.0)
-        s3j = st.number_input("$s_{3j(k)}$ Working efficiently",key='s3j',step=1.0)
-        s4j = st.number_input("$s_{4j(k)}$ Taking a proactive approach",key='s4j',step=1.0)
-
-    if st.button("Skill gap"):
-        ti = np.array([
-            [s1i],
-            [s2i],
-            [s3i],
-            [s4i]
-        ])
-        tj = np.array([
-            [s1j],
-            [s2j],
-            [s3j],
-            [s4j]
-        ])
-        euclidean_distance = 0.45 * np.linalg.norm(ti - tj)
-        st.write(euclidean_distance)
-
-def UNIT4_5():
 
     st.write(
         '''
@@ -238,11 +162,65 @@ def UNIT4_5():
         '''
     )
 
+def UNIT4_4():
+    
+    st.write(
+        '''
+        The **skill gap** between an employee and the job they are performing can be 
+        measured by he Euclidean distance between the **job skill requirements** $s_k$ 
+        and the **employee skill profile** $s_i = (s_{i1}, s_{i2}, s_{i3}, s_{i4})$:
+        '''
+    )
+
+    st.latex(
+        r"""
+        g_i = \sqrt{(\|s_{1k}\| - s_{1i})^2 + (\|s_{2k}\| - s_{2i})^2 + (\|s_{3k}\| - s_{3i})^2 + (\|s_{4k}\| - s_{4i})^2}
+        """
+    )
+
+    st.write(
+        '''
+        As a performance evaluation metric, the **skill gap** is useful for decisions on
+        recruitment, training, and compensation.
+        '''
+    )
+
+    st.markdown("<h3 style='color: #4CAF50;'>🚀 HRM Analytics </h3>", unsafe_allow_html=True)
+    st.sidebar.multiselect("Select two tasks of the job at your firm:",("Intellectual","Physical","Social","Use of methods","Use of technology"))
+    st.sidebar.write('Evaluate the job:')
+    with st.sidebar.expander("$t_i$"):
+        s1i = st.number_input("$s_{1i(k)}$ Demonstrating willigness to learn",key='s1i',step=1.0)
+        s2i = st.number_input("$s_{2i(k)}$ Collaborating in teams and networks",key='s2i',step=1.0)
+        s3i = st.number_input("$s_{3i(k)}$ Working efficiently",key='s3i',step=1.0)
+        s4i = st.number_input("$s_{4i(k)}$ Taking a proactive approach",key='s4i',step=1.0)
+    with st.sidebar.expander("$t_j$"):
+        s1j = st.number_input("$s_{1j(k)}$ Demonstrating willigness to learn",key='s1j',step=1.0)
+        s2j = st.number_input("$s_{2j(k)}$ Collaborating in teams and networks",key='s2j',step=1.0)
+        s3j = st.number_input("$s_{3j(k)}$ Working efficiently",key='s3j',step=1.0)
+        s4j = st.number_input("$s_{4j(k)}$ Taking a proactive approach",key='s4j',step=1.0)
+
+    if st.button("Skill gap"):
+        ti = np.array([
+            [s1i],
+            [s2i],
+            [s3i],
+            [s4i]
+        ])
+        tj = np.array([
+            [s1j],
+            [s2j],
+            [s3j],
+            [s4j]
+        ])
+        euclidean_distance = 0.45 * np.linalg.norm(ti - tj)
+        st.write(euclidean_distance)
+
+
 st.set_page_config(page_title="UNIT 4 Employee Performance Evaluation", layout="wide")
 
 selected = option_menu(
     menu_title="Main Menu",  # required
-    options=['Classification','Labor productivity and ULC','Output elasticity of effort','Skill gap','Cost of effort'],  # required
+    options=['Classification','Labor productivity and ULC','Output elasticity of effort','Cost of effort','Skill gap','Practice 12% (accumulated)'],  # required
     icons=["house", "book", "calculator", "person", "globe"],  # optional
     menu_icon="cast",  # optional
     default_index=0,  # optional
@@ -256,7 +234,9 @@ elif selected == "Labor productivity and ULC":
     UNIT4_2()
 elif selected == "Output elasticity of effort":
     UNIT4_3()
-elif selected == "Skill gap":
-    UNIT4_4()
 elif selected == "Cost of effort":
+    UNIT4_4()
+elif selected == "Skill gap":
     UNIT4_5()
+elif selected == "Practice 12% (accumulated)":
+    UNIT4_6()
