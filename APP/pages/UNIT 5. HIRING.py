f3cfdad5443beb2642c244d0cbc9222ade68035b
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-17 22:18:34 +0100

    Update and rename UNIT 5 Hiring.py to UNIT 5. HIRING.py

---
+++ APP/pages/UNIT 5. HIRING.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from streamlit_option_menu import option_menu
+
+
+
+def UNIT5_5():
+  
+  st.markdown("<h3 style='color: #4CAF50;'>🚀 Practice 16 </h3>", unsafe_allow_html=True)
+
+  st.write(
+    '''
+    - [Skills intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence) from CEDEFOP (European Centre for the 
+    Development of Vocational Training)
+    - [SABI](https://www.unavarra.es/biblioteca?languageId=1) from the library at UPNA
+
+    Choose your firm:
+    1. Access the SABI database through the UPNA Library
+    2. In 'Personalizar' then 'Opciones Generales', change the language to English
+    3. Firm qualification based on **Employees** criteria:
+      - Minimum lastest number of employees of 750
+      - Employees' segmentation in Spain (last available year):
+      - Other managers
+      - Support intellectuals and scientists, technicians and professionals
+      - Administrative employees
+      - At least 10 women
+    4. View list of results
+    5. Select one firm from the firms that qualify
+    '''
+  )
+
+  st.text_input('', placeholder='Write here the name of your firm')
+  st.text_input('Select your firm and then **Overview** under **Industry & overview**',placeholder="Write here your firm's NACE Rev. 2 Primary Code and English trade description")
+                                                                                                   
+  with st.sidebar.expander("Number of employees at your firm from [SABI](https://www.unavarra.es/biblioteca?languageId=1)"):
+    L2020 = st.number_input("2020",key='L2020',value=1.0,step=1.0)
+    L2021 = st.number_input("2021",key='L2021',value=1.0,step=1.0)
+    L2022 = st.number_input("2022",key='L2022',value=1.0,step=1.0)
+
+  L = [L2020,L2021,L2022]
+
+  fig = plt.figure(figsize=(5,5),dpi=100)
+  plt.plot(['2020','2021','2022'],L,color='red',label='Employment')
+  plt.title('Employment over time')
+  plt.legend()
+  st.pyplot(fig)
+    
+st.set_page_config(page_title="UNIT 5. HIRING", layout="wide")
+
+selected = option_menu(
+    menu_title="Main Menu",  # required
+    options=['UNIT 5. HIRING'],  # required
+    icons=["person"],  # optional
+    menu_icon="cast",  # optional
+    default_index=0,  # optional
+    orientation="vertical",
+)
+
+# Call the selected section
+if selected == "Employment":
+    UNIT5_1()
+
