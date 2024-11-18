3906307d78b43568809c3dfaf7954590f5b9e743
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-18 07:28:09 +0100

    Update UNIT 4. EMPLOYEE PERFORMANCE EVALUATION.py

--- APP/pages/UNIT 4. EMPLOYEE PERFORMANCE EVALUATION.py
+++ APP/pages/UNIT 4. EMPLOYEE PERFORMANCE EVALUATION.py
@@ -61,36 +61,6 @@ st.sidebar.write('Evaluate the job:')
         gap = np.sqrt(np.sum((row_norms - np.array([s1,s2,s3,s4])) ** 2))
         st.write(gap)
     
-    st.sidebar.multiselect("Select two tasks of the job at your firm:",("Intellectual","Physical","Social","Use of methods","Use of technology"))
-    st.sidebar.write('Evaluate the job:')
-    with st.sidebar.expander("$t_i$"):
-        s1i = st.number_input("$s_{1i(k)}$ Demonstrating willigness to learn",key='s1i',step=1.0)
-        s2i = st.number_input("$s_{2i(k)}$ Collaborating in teams and networks",key='s2i',step=1.0)
-        s3i = st.number_input("$s_{3i(k)}$ Working efficiently",key='s3i',step=1.0)
-        s4i = st.number_input("$s_{4i(k)}$ Taking a proactive approach",key='s4i',step=1.0)
-    with st.sidebar.expander("$t_j$"):
-        s1j = st.number_input("$s_{1j(k)}$ Demonstrating willigness to learn",key='s1j',step=1.0)
-        s2j = st.number_input("$s_{2j(k)}$ Collaborating in teams and networks",key='s2j',step=1.0)
-        s3j = st.number_input("$s_{3j(k)}$ Working efficiently",key='s3j',step=1.0)
-        s4j = st.number_input("$s_{4j(k)}$ Taking a proactive approach",key='s4j',step=1.0)
-
-    if st.button("Skill gap"):
-        ti = np.array([
-            [s1i],
-            [s2i],
-            [s3i],
-            [s4i]
-        ])
-        tj = np.array([
-            [s1j],
-            [s2j],
-            [s3j],
-            [s4j]
-        ])
-        euclidean_distance = 0.45 * np.linalg.norm(ti - tj)
-        st.write(euclidean_distance)
-
-
 st.set_page_config(page_title="UNIT 4. Employee Performance Evaluation", layout="wide")
 
 selected = option_menu(
