00c7ca180b017b6e38f5d32338ddce9a4852e971
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-18 10:33:15 +0100

    Update UNIT 7. COMPENSATION.py

--- APP/pages/UNIT 7. COMPENSATION.py
+++ APP/pages/UNIT 7. COMPENSATION.py
@@ -7,58 +7,21 @@ from streamlit_option_menu import option_menu
 def UNIT7_1():
  
     st.markdown("<h3 style='color: #4CAF50;'>🚀 Practice 22 </h3>", unsafe_allow_html=True)
-    st.sidebar.radio("Select a job at your firm:",("other managers", "support intellectuals and scientists, technicians and professionals", "sales representatives and similar"))
-    st.sidebar.write('Evaluate the job:')
-    with st.sidebar.expander("Intellectual"):
-        s11 = st.number_input("demonstrating willigness to learn",key='s11',min_value=0,max_value=100,step=1)
-        s21 = st.number_input("collaborating in teams and networks",key='s21',min_value=0,max_value=100,step=1)
-        s31 = st.number_input("working efficiently",key='s31',min_value=0,max_value=100,step=1)
-        s41 = st.number_input("taking a proactive approach",key='s41',min_value=0,max_value=100,step=1)
-    with st.sidebar.expander("Physical"):
-        s12 = st.number_input("demonstrating willigness to learn",key='s12',min_value=0,max_value=100,step=1)
-        s22 = st.number_input("collaborating in teams and networks",key='s22',min_value=0,max_value=100,step=1)
-        s32 = st.number_input("working efficiently",key='s32',min_value=0,max_value=100,step=1)
-        s42 = st.number_input("taking a proactive approach",key='s42',min_value=0,max_value=100,step=1)
-    with st.sidebar.expander("Social"):
-        s13 = st.number_input("demonstrating willigness to learn",key='s13',min_value=0,max_value=100,step=1)
-        s23 = st.number_input("collaborating in teams and networks",key='s23',min_value=0,max_value=100,step=1)
-        s33 = st.number_input("working efficiently",key='s33',min_value=0,max_value=100,step=1)
-        s43 = st.number_input("taking a proactive approach",key='s43',min_value=0,max_value=100,step=1)
-    with st.sidebar.expander("Use of methods"):
-        s14 = st.number_input("demonstrating willigness to learn",key='s14',min_value=0,max_value=100,step=1)
-        s24 = st.number_input("collaborating in teams and networks",key='s24',min_value=0,max_value=100,step=1)
-        s34 = st.number_input("working efficiently",key='s34',min_value=0,max_value=100,step=1)
-        s44 = st.number_input("Ttaking a proactive approach",key='s44',min_value=0,max_value=100,step=1)
-    with st.sidebar.expander("Use of technology"):
-        s15 = st.number_input("demonstrating willigness to learn",key='s15',min_value=0,max_value=100,step=1)
-        s25 = st.number_input("collaborating in teams and networks",key='s25',min_value=0,max_value=100,step=1)
-        s35 = st.number_input("working efficiently",key='s35',min_value=0,max_value=100,step=1)
-        s45 = st.number_input("taking a proactive approach",key='s45',min_value=0,max_value=100,step=1)
+    with st.sidebar.expander("Rate of pay per unit of effort as the median monthly gross income from [Skills intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence"):
+      w = st.number_input("w",key='w',step=1,min_value=0)
+    with st.sidebar.expander("Operating revenue of your firm from [SABI](https://www.unavarra.es/biblioteca?languageId=1)"):
+      OR2020 = st.number_input("2020",key='OR2020',value=1.0,step=1.0)
 
-    st.sidebar.write('Skill profile:')
-    with st.sidebar.expander("Skill profile"):
-        s1 = st.number_input("demonstrating willigness to learn",key='s1',min_value=0,max_value=100,step=1)
-        s2 = st.number_input("collaborating in teams and networks",key='s2',min_value=0,max_value=100,step=1)
-        s3 = st.number_input("working efficiently",key='s3',min_value=0,max_value=100,step=1)
-        s4 = st.number_input("taking a proactive approach",key='s4',min_value=0,max_value=100,step=1)
 
-    matrix = np.array([
-            [s11, s12, s13, s14, s15],
-            [s21, s22, s23, s24, s25],
-            [s31, s32, s33, s34, s35],
-            [s41, s42, s43, s44, s45]
-        ])
-    
-    row_norms = 0.45 * np.linalg.norm(matrix, axis=1)
-    gap = np.sqrt(np.sum((row_norms - np.array([s1,s2,s3,s4])) ** 2))
-    
-    if st.button("Job evaluation"):
-        st.write(matrix)
-    if st.button("Job skill requirements"):
-        for norm in row_norms:
-            st.write(norm)          
-    if st.button("Skill gap"):
-        st.write(gap)
+  LP = [OR2020/L2020,OR2021/L2021,OR2022/L2022]
+  ULC = [C2020/OR2020,C2021/OR2021,C2022/OR2022]
+
+  fig = plt.figure(figsize=(5,5),dpi=100)
+  plt.plot(['2020','2021','2022'],LP,color='red',label='Labor productivity')
+  plt.plot(['2020','2021','2022'],ULC,color='blue',label='ULC')
+  plt.title('HRM metrics over time')
+  plt.legend()
+  st.pyplot(fig)
 
 st.set_page_config(page_title="UNIT 7. COMPENSATION", layout="wide")
 
