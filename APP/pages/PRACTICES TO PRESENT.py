0ae0c7085133da1757877a809cb9d229b29536af
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-11-23 08:10:41 +0100

    Update PRACTICES TO PRESENT.py

--- APP/pages/PRACTICES TO PRESENT.py
+++ APP/pages/PRACTICES TO PRESENT.py
@@ -7,7 +7,6 @@ import numpy as np
 def UNIT0_1():
   
   st.markdown("<h3 style='color: #4CAF50;'>🚀 Practices to present </h3>", unsafe_allow_html=True)
-
   st.write(
     '''
     In this course, students will complete **25 Practices**, each contributing 1% to their final grade. 
@@ -19,9 +18,9 @@ def UNIT0_1():
 
     The table below summarizes the practices by unit and type:
     '''
-)
-
-st.markdown(
+  )
+  
+  st.markdown(
     '''
     | Unit    |  Test & Quiz Practices   | Interactive Practices |
     |---------|--------------------------|-----------------------|
@@ -33,21 +32,17 @@ st.markdown(
     - [Skills intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence) from CEDEFOP (European Centre for the Development of Vocational Training)
     - [SABI](https://www.unavarra.es/biblioteca?languageId=1) from UPNA library
     '''
-)
-
-
-# Set page configuration
-st.set_page_config(page_title="PRACTICES TO PRESENT", layout="wide")
-
-selected = option_menu(
+  )
+  
+  st.set_page_config(page_title="PRACTICES TO PRESENT", layout="wide")
+  selected = option_menu(
     menu_title="",  # required
     options=['PRACTICES TO PRESENT'],  # required
     icons=['person'],  # optional
     menu_icon="cast",  # optional
     default_index=0,  # optional
     orientation="vertical",
-)
-
-# Call the selected section
-if selected == 'PRACTICES TO PRESENT':
+  )
+  
+  if selected == 'PRACTICES TO PRESENT':
     UNIT0_1()
