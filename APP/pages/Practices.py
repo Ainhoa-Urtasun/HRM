b45490ef68a4a5cc1f5209791a6e593b92fb40e9
Ainhoa <ainhoa.urtasun@unavarra.es>
2024-12-05 10:50:12 +0100

    Update Practices.py

--- APP/pages/Practices.py
+++ APP/pages/Practices.py
@@ -8,20 +8,25 @@ def UNIT0_1():
     st.markdown("<h3 style='color: #4CAF50;'>25 in-person activities, each contributing 1% to the final grade:</h3>", unsafe_allow_html=True)
     st.write(
         '''
-        - **In-person activities 1 to 8 and 13 to 19:** Exam-like questions for each unit
-        - **In-person activities 9 to 12:** Mid-semester presentations
-        - **In-person activities 20 to 24:** End-of-semester presentations
-        - **In-person activity 25:** Cohesiveness of the presentations
+
+
+
         
-        Students can do the presentations individually or in pairs.
+        - **In-person activities 1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 23:** Exam-like questions
+        - **In-person activities 3, 6, 8, 12, 18, 21, 24:** presentations
+        - **In-person activities 15 to 25:** Power BI
+        
+        Students should do the presentations in pairs.
 
         The only requirement to participate in in-person activities is to be present in class. 
         No exceptions or justifications for absences will be accepted, regardless of the reason (illness, sports, family, etc.).
 
         **Online Resources**:
         - [MiAulario](https://miaulario.unavarra.es/portal) from UPNA
+        - [HRM App](https://curvp4nsnitb9ukuuholt9.streamlit.app/)
         - [Skills intelligence](https://www.cedefop.europa.eu/en/tools/skills-intelligence) from CEDEFOP (European Centre for the Development of Vocational Training)
         - [SABI](https://www.unavarra.es/biblioteca?languageId=1) from UPNA library
+        - [Power BI](https://vdibroker.unavarra.es/uds/page/login)
         '''
     )
 
