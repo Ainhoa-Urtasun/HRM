b7578bacb01e2d4b0c367ed7a5f5cc178a55d390
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-01-04 18:26:36 +0100

    Update In-Person Practice 12.py

--- APP/pages/In-Person Practice 12.py
+++ APP/pages/In-Person Practice 12.py
@@ -5,59 +5,23 @@ import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
 
 def UNIT4_1():
-
-    st.sidebar.radio("Select a job at your firm:",("other managers", "support intellectuals and scientists, technicians and professionals", "sales representatives and similar"))
-    st.sidebar.write('Evaluate the job:')
-    with st.sidebar.expander("Intellectual"):
-        s11 = st.number_input("demonstrating willigness to learn",key='s11',min_value=0,max_value=100,step=1)
-        s21 = st.number_input("collaborating in teams and networks",key='s21',min_value=0,max_value=100,step=1)
-        s31 = st.number_input("working efficiently",key='s31',min_value=0,max_value=100,step=1)
-        s41 = st.number_input("taking a proactive approach",key='s41',min_value=0,max_value=100,step=1)
-    with st.sidebar.expander("Physical"):
-        s12 = st.number_input("demonstrating willigness to learn",key='s12',min_value=0,max_value=100,step=1)
-        s22 = st.number_input("collaborating in teams and networks",key='s22',min_value=0,max_value=100,step=1)
-        s32 = st.number_input("working efficiently",key='s32',min_value=0,max_value=100,step=1)
-        s42 = st.number_input("taking a proactive approach",key='s42',min_value=0,max_value=100,step=1)
-    with st.sidebar.expander("Social"):
-        s13 = st.number_input("demonstrating willigness to learn",key='s13',min_value=0,max_value=100,step=1)
-        s23 = st.number_input("collaborating in teams and networks",key='s23',min_value=0,max_value=100,step=1)
-        s33 = st.number_input("working efficiently",key='s33',min_value=0,max_value=100,step=1)
-        s43 = st.number_input("taking a proactive approach",key='s43',min_value=0,max_value=100,step=1)
-    with st.sidebar.expander("Use of methods"):
-        s14 = st.number_input("demonstrating willigness to learn",key='s14',min_value=0,max_value=100,step=1)
-        s24 = st.number_input("collaborating in teams and networks",key='s24',min_value=0,max_value=100,step=1)
-        s34 = st.number_input("working efficiently",key='s34',min_value=0,max_value=100,step=1)
-        s44 = st.number_input("Ttaking a proactive approach",key='s44',min_value=0,max_value=100,step=1)
-    with st.sidebar.expander("Use of technology"):
-        s15 = st.number_input("demonstrating willigness to learn",key='s15',min_value=0,max_value=100,step=1)
-        s25 = st.number_input("collaborating in teams and networks",key='s25',min_value=0,max_value=100,step=1)
-        s35 = st.number_input("working efficiently",key='s35',min_value=0,max_value=100,step=1)
-        s45 = st.number_input("taking a proactive approach",key='s45',min_value=0,max_value=100,step=1)
-
-    st.sidebar.write('Skill profile:')
-    with st.sidebar.expander("Skill profile"):
+    st.sidebar.write('Vector of Skills Possessed by the Employee:')
+    with st.sidebar.expander("Vector of Skills Possessed by the Employee"):
         s1 = st.number_input("demonstrating willigness to learn",key='s1',min_value=0,max_value=100,step=1)
         s2 = st.number_input("collaborating in teams and networks",key='s2',min_value=0,max_value=100,step=1)
         s3 = st.number_input("working efficiently",key='s3',min_value=0,max_value=100,step=1)
         s4 = st.number_input("taking a proactive approach",key='s4',min_value=0,max_value=100,step=1)
 
-    matrix = np.array([
-            [s11, s12, s13, s14, s15],
-            [s21, s22, s23, s24, s25],
-            [s31, s32, s33, s34, s35],
-            [s41, s42, s43, s44, s45]
-        ])
+    st.sidebar.write('Vector of Required Skills:')
+    with st.sidebar.expander("Vector of Required Skills"):
+        s1k = st.number_input("demonstrating willigness to learn",key='s1',min_value=0,max_value=100,step=1)
+        s2k = st.number_input("collaborating in teams and networks",key='s2',min_value=0,max_value=100,step=1)
+        s3k = st.number_input("working efficiently",key='s3',min_value=0,max_value=100,step=1)
+        s4k = st.number_input("taking a proactive approach",key='s4',min_value=0,max_value=100,step=1)
     
-    row_norms = (1/np.sqrt(5)) * np.linalg.norm(matrix, axis=1)
-    gap = np.sqrt(np.sum((row_norms - (1/np.sqrt(5))*np.array([s1,s2,s3,s4])) ** 2))
-    
-    if st.button("Matrix of Tasks and Required Skills"):
-        st.write(matrix)
-    if st.button("Vector of Required Skills (Norms)"):
-        for norm in row_norms:
-            st.write(norm)          
-    if st.button("Skill Gap"):
-        st.write(gap)
+    gap = np.sqrt(np.sum((1/np.sqrt(5))*np.array([s1k,s2k,s3k,s4k]) - (1/np.sqrt(5))*np.array([s1,s2,s3,s4])) ** 2))
+
+(1/np.sqrt(5))*np.array([s1,s2,s3,s4]
             
 st.set_page_config(page_title="In-Person Practice 12", layout="wide")
 
