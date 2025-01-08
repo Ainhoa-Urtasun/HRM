1d06ccc86c8cad1bb0ebbb8401c7b7410d4aa645
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-01-08 08:19:00 +0100

    Update In-Person Practice 15.py

--- APP/pages/In-Person Practice 15.py
+++ APP/pages/In-Person Practice 15.py
@@ -22,8 +22,8 @@ def UNIT5_1():
     y_values = g * e_values**2
     ax.plot(e_values, y_values, label=f"C(e_i) = {g}e_i^2", linewidth=2)
   ax.axhline(y=w_probation, color='red', linestyle='--', linewidth=2, label=f"w_probation = {w_probation}")
-  ax.set_xlabel("Effort ($e_i$)", fontsize=14)
-  ax.set_ylabel("Cost (C($e_i$))", fontsize=14)
+  ax.set_xlabel("$e_i$", fontsize=14)
+  ax.set_ylabel($C(e_i)$", fontsize=14)
   ax.legend(fontsize=12)
   ax.grid(True)
   st.pyplot(fig)
