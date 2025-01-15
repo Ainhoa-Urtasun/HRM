5d4612e228dcda676e163ff3bab016101c2a2c43
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-01-15 10:52:16 +0100

    Update HRM Report.py

--- APP/pages/HRM Report.py
+++ APP/pages/HRM Report.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.linalg import norm
 
-
+def report():
 user_text = st.text_area("Your text here:", placeholder="Type something...")
 st.set_page_config(page_title="HRM Report", layout="wide")    
 
