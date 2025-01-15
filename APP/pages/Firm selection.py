94408804bdeed51dc74bcbc58087d98bfffaa72b
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-01-15 11:04:01 +0100

    Update and rename HRM Report.py to Firm selection.py

---
+++ APP/pages/Firm selection.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.linalg import norm
+
+def firm_selection():
+  user_text = st.text_area("Your text here:", placeholder="Type something...")
+
+st.set_page_config(page_title="Firm selection", layout="wide")
+
+# Navigation menu
+selected = option_menu(
+    menu_title="",  # No title for the menu
+    options=["Firm selection"],  # Menu options
+    icons=["book"],  # Icons for options
+    menu_icon="cast",  # Icon for the menu
+    default_index=0,  # Default selected option
+    orientation="vertical",
+)
+
+# Call the selected section
+if selected == "Firm selection":
+    firm_selection()
+
+
+
+
