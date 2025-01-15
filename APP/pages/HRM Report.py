c8f05825dc623e69f3414ecf4a00dea9136b16d9
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-01-15 10:51:36 +0100

    Update HRM Report.py

--- APP/pages/HRM Report.py
+++ APP/pages/HRM Report.py
@@ -5,18 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.linalg import norm
 
-st.set_page_config(page_title="HRM Report", layout="wide")
+
 user_text = st.text_area("Your text here:", placeholder="Type something...")
-    
+st.set_page_config(page_title="HRM Report", layout="wide")    
 
-
-# Navigation menu
-selected = option_menu(
-    menu_title="",  # No title for the menu
-    options=["HRM Report"],  # Menu options
-    icons=["book"],  # Icons for options
-    menu_icon="cast",  # Icon for the menu
-    default_index=0,  # Default selected option
-    orientation="vertical",
-)
 
