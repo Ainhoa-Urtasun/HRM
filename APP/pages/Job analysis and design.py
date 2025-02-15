939151b0ff60faf9b7735588fadde6ff2f6b7616
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-02-15 09:40:25 +0100

    Update Job analysis and design.py

--- APP/pages/Job analysis and design.py
+++ APP/pages/Job analysis and design.py
@@ -52,7 +52,7 @@ def job_analysis_and_design():
     max_values = np.max(matrix, axis=0)
     st.write("Skill requirements of the job:", max_values)
 
-    st.text_area("", placeholder="Describe the connection between your firm and the evaluated job")
+    st.text_area("", placeholder="Connect the Dots: Imagine you are the HR manager of this firm. Please explain how the economic activities of the firm are connected to the tasks of the job you have just evaluated.")
     st.text_area("", placeholder="What would be the skill gap of an employee with 25 of Demonstrating willingness to learn, 50 of Collaborating in teams and networks, 12 of Working efficiently, and 5 of Taking a proactive approach? Explain the characteristics and effects of such skill gap")
 
 # Streamlit page setup
