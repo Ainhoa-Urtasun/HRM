3d02574d7e8900a34d8296604a887b4d91ade344
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-02-23 20:59:08 +0100

    Update Hiring.py

--- APP/pages/Hiring.py
+++ APP/pages/Hiring.py
@@ -7,7 +7,7 @@ from streamlit_option_menu import option_menu
 def hiring():
 
   st.sidebar.header("Designing probation")
-  skill_gaps_input = st.sidebar.text_area("Enter possible skill gaps (one per row, without commas):","")
+  skill_gaps_input = st.sidebar.text_area("Enter several possible skill gaps (one per row, without commas):","")
   try:
     skill_gaps = [float(g.strip()) for g in skill_gaps_input.split("\n") if g.strip()]
   except ValueError:
