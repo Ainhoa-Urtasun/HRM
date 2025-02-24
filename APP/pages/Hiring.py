d10eea04fa74cf257e0dc91a3325d6cd95cbde5e
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-02-24 11:24:45 +0100

    Update Hiring.py

--- APP/pages/Hiring.py
+++ APP/pages/Hiring.py
@@ -13,7 +13,7 @@ def hiring():
   except ValueError:
     st.error("Please enter valid numeric values for skill gaps.")
     skill_gaps = []
-  w_probation = st.sidebar.slider("Set probation wage (horizontal line):", 0, 5000, 1, 1)
+  w_probation = st.sidebar.slider("Set probation wage:", 0, 5000, 1, 1)
   
   st.text_area("", placeholder="Explain signaling as an HRM practice")
   st.text_area("", placeholder="Explain screening as an HRM practice")
