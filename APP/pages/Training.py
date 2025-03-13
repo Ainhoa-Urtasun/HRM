bf9a3dd6b8471269793ea2426575fde827f3bdb4
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-03-13 11:33:59 +0100

    Update Training.py

--- APP/pages/Training.py
+++ APP/pages/Training.py
@@ -6,10 +6,10 @@ from streamlit_option_menu import option_menu
 
 def Training():
 
-    with st.sidebar.expander("Skill value (0-10) of the trained 'Support intellectuals and scientists, technicians and professionals'"):
+    with st.sidebar.expander("Skill Level of Trained 'Support intellectuals and scientists, technicians and professionals' (Scale: 0-100)"):
         t0 = st.number_input("Before training", key="t0", step=1)
         t1 = st.number_input("After training", key="t1", step=1)
-    with st.sidebar.expander("Skill value (0-100) of the non-trained 'Support intellectuals and scientists, technicians and professionals'"):
+    with st.sidebar.expander("Skill Level of Trained 'Support intellectuals and scientists, technicians and professionals' (Scale: 0-100)"):
         n0 = st.number_input("Before training", key="n0", step=1)
         n1 = st.number_input("After training", key="n1", step=1)
 
@@ -25,8 +25,8 @@ def Training():
     st.text_area("", placeholder="Make your pitch compelling...") 
   
     fig = plt.figure(figsize=(5,5),dpi=100)
-    plt.plot(['Before training','After training'],[t0,t1],color='red',label="Trained employee's skill")
-    plt.plot(['Before training','After training'],[n0,n1],color='blue',label="Non-trained employee's skill")
+    plt.plot(['Before training','After training'],[t0,t1],color='red',label="Skill Level of Trained Employees (Scale: 0-100)")
+    plt.plot(['Before training','After training'],[n0,n1],color='blue',label="Skill Level of Non-Trained Employees (Scale: 0-100)")
     plt.plot(['Before training','After training'],[t0,t0+(n1-n0)],color='green',ls='-.',label='Counterfactual')
     plt.title('Assessing Training Impact with DiD')
     plt.legend()
