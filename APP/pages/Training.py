6d6075a78ec1d64e8da939914acf1c47c5d49c06
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-03-13 11:30:53 +0100

    Update Training.py

--- APP/pages/Training.py
+++ APP/pages/Training.py
@@ -24,11 +24,10 @@ def Training():
 
     st.text_area("", placeholder="Make your pitch compelling...") 
   
-    st.text_area("", placeholder="Write your training plan here.")  
     fig = plt.figure(figsize=(5,5),dpi=100)
-    plt.plot(['-1','+1'],[t0,t1],color='red',label="Trained employee's skill")
-    plt.plot(['-1','+1'],[n0,n1],color='blue',label="Non-trained employee's skill")
-    plt.plot(['-1','+1'],[t0,t0+(n1-n0)],color='green',ls='-.',label='Counterfactual')
+    plt.plot(['Before training','After training'],[t0,t1],color='red',label="Trained employee's skill")
+    plt.plot(['Before training','After training'],[n0,n1],color='blue',label="Non-trained employee's skill")
+    plt.plot(['Before training','After training'],[t0,t0+(n1-n0)],color='green',ls='-.',label='Counterfactual')
     plt.title('Assessing Training Impact with DiD')
     plt.legend()
     st.pyplot(fig)
