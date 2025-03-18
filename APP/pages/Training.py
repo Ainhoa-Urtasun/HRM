05d8efcdecf0d10912e49c5e39eb0fed95c13d25
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-03-18 07:45:53 +0100

    Update Training.py

--- APP/pages/Training.py
+++ APP/pages/Training.py
@@ -17,7 +17,7 @@ def Training():
     ### Designing On-the-Job Training
     
     1. Describe what the values on your left represent  
-    2. Leverage the visual to provide strong evidence of the expected success of on-the-job training
+    2. Leverage the visual to provide strong evidence of the expected success of on-the-job training 
     3. Craft a persuasive pitch that convinces others of the value your training delivers!  
     """)  
 
