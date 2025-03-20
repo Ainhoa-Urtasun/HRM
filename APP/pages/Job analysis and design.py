2ca6411a8d2cda4c2a903b340c820599f2541298
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-03-20 21:37:22 +0100

    Update Job analysis and design.py

--- APP/pages/Job analysis and design.py
+++ APP/pages/Job analysis and design.py
@@ -55,7 +55,7 @@ def job_analysis_and_design():
 
     st.write("""
     #### Question 4: 
-    What will be the effects of the implementation of the new technology on the workers' skill gap? Explain
+    What will be the effects of the implementation of the new technology on the workers' skill gap? Is there something the firm should do? Explain
     """)
     st.text_area("", placeholder="Write the answer to Question 4 here...") 
  
