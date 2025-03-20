c494cbebaa11386589852be01c3b79f0317e6047
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-03-20 20:42:13 +0100

    Update Firm selection.py

--- APP/pages/Firm selection.py
+++ APP/pages/Firm selection.py
@@ -25,8 +25,12 @@ def firm_selection():
     Power BI, a highly demanded software developed by Microsoft, enables businesses to make data-driven decisions by seamlessly integrating and visualizing data. 
     Your firm has decided to implement this powerful tool. Describe how Power BI can support your firm in the performance of its economic activities activities
     """)
-    st.text_area("", placeholder="Write here...") 
-    st.text_area("Question 2: Which job within your firm, do you think, should master the new technology? Why?", placeholder="Write here...") 
+    st.text_area("", placeholder="Write here...")
+    st.write("""
+    #### Question 2:
+    Which job within your firm should master the new technology? Why?
+    """)
+    st.text_area("Question 2: ", placeholder="Write here...") 
 
 st.set_page_config(page_title="Firm Selection", layout="wide")
 
