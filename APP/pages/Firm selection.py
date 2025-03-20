dda9b7ddce198a5aac967585af2b4695496f253a
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-03-20 20:33:15 +0100

    Update Firm selection.py

--- APP/pages/Firm selection.py
+++ APP/pages/Firm selection.py
@@ -25,6 +25,7 @@ def firm_selection():
     Your firm has decided to implement this powerful tool. Describe how Power BI can support your firm in the performance of its economic activities activities.
     """)
     st.text_area("", placeholder="Write here...") 
+    st.write("""
     ### 2. Which job within your firm, do you think, should master the new technology? Why?
     """)
     st.text_area("", placeholder="Write here...") 
