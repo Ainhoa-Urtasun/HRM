2e1d5cc6a3c314b8410946a3e91a7eacc8f2ce54
Ainhoa <ainhoa.urtasun@unavarra.es>
2025-06-28 23:07:30 +0200

    Update and rename Labor Supply.py to Isoquants.py

---
+++ APP/pages/Isoquants.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from streamlit_option_menu import option_menu
+
+def Isoquants():
+    st.title("Isoquants")
+
+    # Sidebar: Parameters
+    w = st.sidebar.slider("w (Pay per hour of work)", min_value=1.0, max_value=20.0, value=20.0, step=1.0)
+
+    # Days off
+    H = np.linspace(1, 10, 200)
+
+    # Plot setup
+    plt.figure(figsize=(9, 6))
+
+    # Isoquants
+    plt.plot(H, 9 - np.log(H), label=f'Isoquant: U=9')
+    plt.plot(H, 10 - np.log(H), label=f'Isoquant: U=10')
+    plt.plot(H, 11 - np.log(H), label=f'Isoquant: U=11')
+            
+    # Time restriction:
+    w = 0.4
+    plt.plot(H, w * (24 - H), label=f'Time restriction w = 0.4')
+
+    # Final touches
+    plt.xlabel('Hours off (H)')
+    plt.ylabel('Income (wL)')
+    plt.grid(True)
+    plt.legend()
+    plt.xlim(0, 10)
+
+    st.pyplot(plt)
+
+# Streamlit App Setup
+st.set_page_config(page_title="Labor Supply", layout="wide")
+
+# Navigation
+selected = option_menu(
+    menu_title="",
+    options=["Isoquants"],
+    icons=["person"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="vertical"
+)
+
+if selected == "Isoquants":
+    Isoquants()
